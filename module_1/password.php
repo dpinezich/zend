bdd4edfd3665bc1e6315b0b3a4b188ee12bcd473
dpinezich <david.pinezich@gmail.com>
2016-11-08 23:37:57 +0100

    checkhash and stuff

--- module_1/password.php
+++ module_1/password.php
@@ -1,13 +1,19 @@
 <?php
 
 $password = ($_GET['password']) ?: '';
+$checkHash = '$2y$10$UtDeUAD8Fdhq.nrn69QWJessybqhOf1ZxOGVtTlPLmiQhGs69PujS';
 
 $passHash = password_hash('secret', PASSWORD_DEFAULT);
 
 if (password_verify($password, $passHash))
 {
     //Right Password
-    echo "password is gud";
+    echo "password is good, hash: " . $passHash;
+    echo "<br />Some password_get_info: <br />";
+    echo "<pre>" . print_r(password_get_info($passHash)) . "</pre>";
+    echo "<br />needs rehash?<br />";
+    echo password_needs_rehash($passHash, PASSWORD_BCRYPT) ? "yes" : "no";
+    echo "<br />checkhash is also guud? " . password_verify($password, $checkHash);
 }
 else
 {
